8f4ffd7a9e624c871885602c7af41132ca2ae33f
Mark Hopcraft <mark.hopcraft@madgex.com>
2020-11-29 22:07:08 +0000

    ad lint fix npm script

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  return res.status(200).json({ message: 'Message here' });
+router.get("/", (req, res) => {
+  return res.status(200).json({ message: "Message here" });
 });
 
 module.exports = router;
